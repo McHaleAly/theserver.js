00687329bed1d1b8730bac16b78680391307ee6d
AlysonAtwell <123188201+McHaleAly@users.noreply.github.com>
2023-02-14 09:42:56 -0500

    Main

--- server.js
+++ server.js
@@ -1,8 +1,47 @@
 const http= require('http');
+const fs = require ('fs')
 
 const server = http.createServer((req, res) => {
-  console.log('request made');
+  console.log(req.url, req.method);
+
+  // set header content type
+  res.setHeader('Content-Type', 'text/html');
+
+  let path='./views/';
+  switch(req.url) {
+    case '/':
+      path+='index.html'
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path=='about.html';
+      res.statusCode = 200;
+      break;
+    case '/about-me':
+      res.statusCode = 301; 
+      res.setHeader('Location', '/about');
+      res.end();
+        break;
+      default:
+        path+= '404.html';
+        res.statusCode = 404;
+        break;
+  }
+
+  // send an html file - pass 2 arguments
+  fs.readFile(path,(err, data) =>{
+    if (err) {
+    console.log(err);
+    res.end() // end so it doesn't hang
+    } else {
+     // res.write(data); since we use one me we put it directly in end method
+     res.statusCode = 200; 
+     res.end(data);
+    }
+  })
 });
+
+
 server.listen(3000,"localhost", () => {
-  console.log('listeing for requests on port 3000');
+  console.log('listening for requests on port 3000');
 });
